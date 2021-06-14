9a95be679047cc288f2cfbea605c99bf3fa56d47
MOLY Jean-Marc <jeanmarc.moly@gmail.com>
2021-06-14 22:06:13 +0200

    fixe date search on convention

--- mugimugi_client_api/search_item/convention.py
+++ mugimugi_client_api/search_item/convention.py
@@ -1,16 +1,21 @@
 from dataclasses import dataclass, field
 from datetime import date
+from enum import Enum
 from typing import Iterator, Optional, Type, Union
 
 from mugimugi_client_api_entity import Convention
 from mugimugi_client_api_entity import SearchConvention as Root
 from mugimugi_client_api_entity.enum import ElementPrefix, ItemType
+from mugimugi_client_api_entity.util.converter.date import DateConverter
 
 from .abstract import SearchItem
 
 
 @dataclass
 class SearchConvention(SearchItem):
+
+    class Parameter(Enum):
+        DATE = "date"  # str YYYY-MM-DD, limited to convention
     root: Type = field(default=Root, init=False)
     type_: ItemType = field(default=ItemType.CONVENTION, init=False)
     date_: Optional[date] = None
@@ -24,4 +29,4 @@ class SearchConvention(SearchItem):
         yield from super().params()
 
         if (date_ := self.date_) is not None:
-            yield self.Parameter.DATE.value, f"{date_:Date.FORMAT}"
+            yield SearchConvention.Parameter.DATE.value, f"{date_:{DateConverter.FORMAT}}"
